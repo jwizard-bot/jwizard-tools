94d9f2ea9edef02aaf10580de6fdf5724fe60143
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-11-02 05:54:39 +0100

    init file for db_migrator

--- src/db_migrator.py
+++ src/db_migrator.py
@@ -1,13 +1,28 @@
-#
-# Copyright (c) 2024 by JWizard
-# Originally developed by Miłosz Gilga <https://miloszgilga.pl>
-#
+"""
+Copyright (c) 2024 by JWizard
+Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+"""
+from logging import info
+from common.logger import *
+from common.header import print_header
 from common.vault import VaultClient
 from common.arg_parser import ArgParser
+from common.db import Db
 
 if __name__ == '__main__':
+  print_header(initiator=__file__)
+
   arg_parser = ArgParser()
   args = arg_parser.get_args()
 
   vault_client = VaultClient(args)
   secrets = vault_client.get_secrets(kv_backend="jwizard", path="common")
+
+  db = Db(
+    host=secrets["V_MYSQL_HOST"],
+    username=secrets["V_MYSQL_USERNAME"],
+    password=secrets["V_MYSQL_PASSWORD"],
+    db_name=secrets["V_MYSQL_DB_NAME"],
+  )
+
+  info(f"Finished.")
