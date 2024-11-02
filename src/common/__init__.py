97a2232d183fe38dfb68cb292587d79ccef3a941
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-11-02 05:56:03 +0100

    change header comment type

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -1,4 +1,4 @@
-#
-# Copyright (c) 2024 by JWizard
-# Originally developed by Miłosz Gilga <https://miloszgilga.pl>
-#
+"""
+Copyright (c) 2024 by JWizard
+Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+"""
